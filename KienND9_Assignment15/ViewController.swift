ae6aa9a6ba19dd4008820a5e7e6e9e46fdd235a9
Kien Nguyen Dang <kiennguyendang@Kiens-Mac.local>
2016-12-04 22:25:01 +0700

    Initial Commit

---
+++ KienND9_Assignment15/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KienND9_Assignment15
+//
+//  Created by Kien Nguyen Dang on 12/4/16.
+//  Copyright © 2016 Kien Nguyen Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
